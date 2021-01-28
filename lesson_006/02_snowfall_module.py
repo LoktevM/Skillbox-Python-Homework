c65403522c9a0899dcf0547c5e4bf8fee09b287d
Михаил <IDM2001flourish@yandex.ru>
2021-01-28 18:22:15 +0300

    Практика 7 урок - снегопад

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -1,6 +1,31 @@
 # -*- coding: utf-8 -*-
-
 import simple_draw as sd
+
+from snowfall import create_snowflakes, draw_snowflakes, move_snowflakes, get_numbers_fallen_snowflakes, \
+    delete_snowflakes
+
+count_sl = 40
+create_snowflakes(count_snowflakes=count_sl)
+
+while True:
+    draw_snowflakes(color=sd.background_color)
+    move_snowflakes()
+    draw_snowflakes(color=sd.COLOR_WHITE)
+
+    numbers_snowflakes=get_numbers_fallen_snowflakes()
+
+    count_news=len(numbers_snowflakes)
+
+    if len(numbers_snowflakes)>0:
+        print(numbers_snowflakes)
+        delete_snowflakes(numbers_snowflakes)
+        create_snowflakes(count_snowflakes=count_news)
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
+
+sd.pause()
 
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
@@ -14,15 +39,15 @@ import simple_draw as sd
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
 # создать_снежинки(N)
-while True:
-    #  нарисовать_снежинки_цветом(color=sd.background_color)
-    #  сдвинуть_снежинки()
-    #  нарисовать_снежинки_цветом(color)
-    #  если есть номера_достигших_низа_экрана() то
-    #       удалить_снежинки(номера)
-    #       создать_снежинки(count)
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+# while True:
+#     #  нарисовать_снежинки_цветом(color=sd.background_color)
+#     #  сдвинуть_снежинки()
+#     #  нарисовать_снежинки_цветом(color)
+#     #  если есть номера_достигших_низа_экрана() то
+#     #       удалить_снежинки(номера)
+#     #       создать_снежинки(count)
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
 
 sd.pause()
